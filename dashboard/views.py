cebf97d7c9426a6cdb75f4709d46c940f2c32661
KenWang <ken0711.wang@gmail.com>
2023-06-11 20:07:58 +0800

    dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
-def hello_world(request):
-    return HttpResponse("Hello World!")
+def dashboard_view(request):
+    # 在這裡處理從後端獲取數據等相關邏輯
+    context = {
+        # 將需要傳遞到模板中的變數添加到context字典中
+        'total_sales': 10000,
+        'best_selling_product': 'Product A',
+        # 其他變數...
+    }
+    return render(request, 'dashboard.html', context)

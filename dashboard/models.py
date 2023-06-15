869d1387163a50c8d8d5789749f6442beee2f8dc
KenWang <ken0711.wang@gmail.com>
2023-06-15 17:23:30 +0800

    signup

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
 class User(AbstractUser):
     birthday = models.DateField(null=True)
-    street = models.CharField(max_length=100)
-    house_number = models.CharField(max_length=10)
-    town = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
+    street = models.CharField(null=True, max_length=100)
+    house_number = models.CharField(null=True, max_length=10)
+    town = models.CharField(null=True, max_length=100)
+    city = models.CharField(null=True, max_length=100)
     news_notification = models.BooleanField(default=False)
     activity_notification = models.BooleanField(default=False)
     promotion_notification = models.BooleanField(default=False)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,43 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from .forms import ProfileForm, NotificationForm
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import ProfileForm, NotificationForm,RegistrationForm
+
 
 # Create your views here.
+class SignUp_View(generic.CreateView):
+    form_class = RegistrationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+
+
+def signup(request):
+    user = request.user
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST,instance=user)
+        if form.is_valid():
+            # 檢查使用者名稱是否已存在
+            username = form.cleaned_data.get('username')
+            if User.objects.filter(username=username).exists():
+                messages.error(request, '該使用者名稱已被使用。請選擇其他使用者名稱。')
+            else:
+                print(form.errors)
+                form.save()
+                messages.success(request, '註冊成功！')
+                return redirect('home')
+        else:
+            print(form.errors)
+            messages.error(request, '註冊失敗。請檢查輸入內容。')
+    else:
+        print(form.errors)
+        form = RegistrationForm(instance=user)
+
+    return render(request, 'registration/signup.html', {'form': form})
+
 @login_required
 def dashboard_view(request):
     # 在這裡處理從後端獲取數據等相關邏輯
@@ -39,7 +73,6 @@ def update_profile(request):
             profile_form.save()
             messages.success(request, "成功更新個人資料。")
         else:
-            print(profile_form.errors)
             messages.error(request, "無法更新個人資料。")
     else:
         profile_form = ProfileForm(instance=user)

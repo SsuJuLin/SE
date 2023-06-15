a2621910cfa595e4ef99df73ea19b45591d78467
OdetteHsu <a09664489992144@gmail.com>
2023-06-15 23:33:10 +0800

    models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import Sum
 
 
 # Create your models here.
@@ -9,10 +10,10 @@ class User(AbstractUser):
     house_number = models.CharField(null=True, max_length=10)
     town = models.CharField(null=True, max_length=100)
     city = models.CharField(null=True, max_length=100)
-    news_notification = models.BooleanField(null=True,default=False)
-    activity_notification = models.BooleanField(null=True,default=False)
-    promotion_notification = models.BooleanField(null=True,default=False)
-    
+    news_notification = models.BooleanField(null=True, default=False)
+    activity_notification = models.BooleanField(null=True, default=False)
+    promotion_notification = models.BooleanField(null=True, default=False)
+
     def __str__(self):
         return self.username
 
@@ -23,7 +24,9 @@ class Product(models.Model):
     @classmethod
     def get_sales_chart_data(cls):
         # 獲取產品收入數據並處理成適合圖表的格式
-        # ...
+        sales_data = cls.objects.values('month').annotate(total_sales=Sum('sales')).order_by('month')
+        chart_labels = [data['month'] for data in sales_data]
+        chart_data = [data['total_sales'] for data in sales_data]
 
         return {
             'labels': chart_labels,
@@ -33,7 +36,9 @@ class Product(models.Model):
     @classmethod
     def get_stock_chart_data(cls):
         # 獲取產品庫存數據並處理成適合圖表的格式
-        # ...
+        stock_data = cls.objects.values('category').annotate(total_stock=Sum('stock')).order_by('category')
+        chart_labels = [data['category'] for data in stock_data]
+        chart_data = [data['total_stock'] for data in stock_data]
 
         return {
             'labels': chart_labels,
@@ -41,3 +46,15 @@ class Product(models.Model):
         }
 
     # 添加其他圖表所需的模型方法或管理器方法
+
+    @classmethod
+    def get_sales_line_chart_data(cls):
+        # 獲取銷售折線圖數據並處理成適合圖表的格式
+        sales_data = cls.objects.values('month').annotate(total_sales=Sum('sales')).order_by('month')
+        chart_labels = [data['month'] for data in sales_data]
+        chart_data = [data['total_sales'] for data in sales_data]
+
+        return {
+            'labels': chart_labels,
+            'data': chart_data,
+        }

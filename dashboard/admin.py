6b2f916395c9f6eb4bd4ba8dadb1aaa25676a8be
KenWang <ken0711.wang@gmail.com>
2023-06-16 19:49:09 +0800

    admin customer

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Product, Order, OrderItem
+from .models import User, Product, Order, OrderItem, Customer
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ admin.site.register(User)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Customer)
